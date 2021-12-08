e1977f06de921d54eaa33d66de3b66803da6271f
richad.97 <rcastillo2022@gmail.com>
2021-12-07 20:32:45 -0800

    add and delete movie functionality work through redux

--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -1,34 +1,43 @@
-import React from 'react';
+import React from "react";
 
-import MovieListItem from './MovieListItem';
-import MovieFooter from './MovieFooter';
+import { connect } from "react-redux";
 
-const MovieList = (props)=> {
-    const movies = [];
+import MovieListItem from "./MovieListItem";
+import MovieFooter from "./MovieFooter";
 
-    return (
-        <div className="col">
-            <table className="table table-striped table-hover">
-                <thead>
-                <tr>
-                    <th>Title</th>
-                    <th>Director</th>
-                    <th>Genre</th>
-                    <th>Metascore</th>
-                    <th></th>
-                </tr>
-                </thead>
+const MovieList = (props) => {
+  const movies = props.movies;
 
-                <tbody>
-                    {
-                        movies.map(movie=><MovieListItem key={movie.id} movie={movie}/>)
-                    }
-                </tbody>
-            </table>
-            
-            <MovieFooter totalMovies={movies.length}/>
-        </div>
-    );
-}
+  return (
+    <div className="col">
+      <table className="table table-striped table-hover">
+        <thead>
+          <tr>
+            <th>Title</th>
+            <th>Director</th>
+            <th>Genre</th>
+            <th>Metascore</th>
+            <th></th>
+          </tr>
+        </thead>
 
-export default MovieList;
+        <tbody>
+          {movies.map((movie) => (
+            <MovieListItem key={movie.id} movie={movie} />
+          ))}
+        </tbody>
+      </table>
+
+      <MovieFooter totalMovies={movies.length} />
+    </div>
+  );
+};
+
+const mapStateToProps = (state) => {
+  return {
+    movies: state.movies,
+    appTitle: state.appTitle,
+  };
+};
+
+export default connect(mapStateToProps, {})(MovieList);
